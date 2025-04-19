18cd458718343c1c616da5800a37ea109154fbc4
Nathan Avery <nwavette@gmail.com>
2025-04-19 03:55:27 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,11 @@ import { SubmitToolComponent } from './pages/submit-tool/submit-tool.component';
 
 export const routes: Routes = [
   { path: '', component: MainPageComponent },
-  { path: 'tool/:id', component: ToolDetailComponent },
+  {
+    path: 'tool/:id',
+    component: ToolDetailComponent,
+    data: { renderMode: 'client' }
+  },
   { path: 'submit', component: SubmitToolComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
